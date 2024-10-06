197eb10a1e96b28b1b54a304a5ac6b1d4f143fe1
Bakhti Khaled <Bakhti.Khaled@hotmail.com>
2024-10-07 00:47:05 +0100

    Fix login function

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:urgence_app/firebase_options.dart';
 import 'package:urgence_app/view/screens/auth/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Urgence Help -Khaled',
+      title: 'Help App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
