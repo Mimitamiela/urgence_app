9bdecb7fd68a9847bc4bd4a0e32a7ff8e5bcc369
mimi <marouakhelladi@gmail.com>
2024-10-09 21:08:17 +0100

    add image in assistanse_screen

--- lib/view/assistance_home_screen.dart
+++ lib/view/assistance_home_screen.dart
@@ -7,9 +7,20 @@ class AssistanceHomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         title: const Text('ASSISTANCE HOME'),
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: AssetImage("assets/images/7N2O.gif"),
+          ),
+        ),
       ),
-      body: Container(),
+      body: Container(
+        color: const Color.fromARGB(255, 250, 232, 232),
+        child: const Center(child: Card()),
+      ),
     );
   }
 }
